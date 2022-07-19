aa71936343b5ec8588e23188a8d8dc738bab1b4a
rausyanfikar <rauzyanfikar@gmail.com>
2022-07-19 16:24:09 +0700

    favorite get,post

--- src/pages/MyFavorite.jsx
+++ src/pages/MyFavorite.jsx
@@ -1,14 +1,42 @@
-import React from "react";
-import Layout from "../components/Layout";
-import CardMyfavorite from "../components/CardMyfavorite";
+import React from 'react';
+import Layout from '../components/Layout';
+import CardMyfavorite from '../components/CardMyfavorite';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MyFavorite = () => {
+  const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    getFavorite();
+  }, []);
+
+  const getFavorite = () => {
+    axios({
+      method: 'get',
+      url: 'https://group3.altaproject.online/favourites',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    })
+      .then((res) => {
+        console.log(res.data.data);
+        setFavourites(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Layout>
       <div className="h-screen">
         <h1 className="text-2xl font-bold md:ml-12 pt-5 mb-3">My Favorite</h1>
         <div className="flex flex-col justify-center">
-          <CardMyfavorite />
+          {favourites.map((item, index) => (
+            <CardMyfavorite key={index} title={item.resto_name} location={item.location} rating={item.rating} category={item.category} />
+          ))}
         </div>
       </div>
     </Layout>
